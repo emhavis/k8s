96201025929fd6194f225d387ab85acb1e631e47
emhavis <emhavis@gmail.com>
2021-08-31 16:22:53 +0000

    script created

---
+++ script/pre-install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+#
+clear
+echo '----------------------------------------------------------------------------------'
+echo 'This script will prepare your cloud server to enable kubernetes installation'
+echo '1. apt update and upgrade'
+echo '2. turn swap off permanently'
+echo '3. install net tools'
+echo '----------------------------------------------------------------------------------'
+read -p "Please review the parameter and Press any key to continue or ctrl-C to cancel"
+echo ""
+echo ""
+# Update the installation
+sudo apt update && sudo apt upgrade
+#
+# turn off swap
+sudo swapoff -a
+#
+#
+# disable the file swap permanently
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+#
+#
+# install net tools
+sudo apt install net-tools
+#
+# 
+# rename hostname, you might need to input password
+hostnamectl set-hostname kmaster
+hostname -F /etc/hostname
