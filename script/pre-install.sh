2b0f4b3bfa29121dc266ce29bc7ce3c46a5d259c
emhavis <emhavis@gmail.com>
2021-08-31 16:32:01 +0000

    script update

--- script/pre-install.sh
+++ script/pre-install.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 #
+# Parameters
+# $1 ->> Hostname
 clear
 echo '----------------------------------------------------------------------------------'
 echo 'This script will prepare your cloud server to enable kubernetes installation'
 echo '1. apt update and upgrade'
 echo '2. turn swap off permanently'
 echo '3. install net tools'
+echo "4. set hostname to $1"
 echo '----------------------------------------------------------------------------------'
 read -p "Please review the parameter and Press any key to continue or ctrl-C to cancel"
 echo ""
@@ -26,5 +29,7 @@ sudo apt install net-tools
 #
 # 
 # rename hostname, you might need to input password
-hostnamectl set-hostname kmaster
+sudo hostnamectl set-hostname $1
 hostname -F /etc/hostname
+#
+#
